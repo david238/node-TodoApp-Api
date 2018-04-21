9be3b6bfadf1cb018ec4a3d0e51608bef9db1c1d
davidtin <davidtin@opdaconsulting.com>
2018-04-21 18:57:32 -0400

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -12,17 +12,28 @@ var {ResumeModel} = require('./models/Resume');
 var app = express();
 
 app.use(bodyParser.json());
+
 app.post('/todos', (req, res) => {
-var todo = new Todo({
-  text: req.body.text
-});
-todo.save().then((doc)=>{
-res.send(doc);
-}, (err) => {
-  res.status(400).send(err);
-});
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((doc)=>{
+  res.send(doc);
+  }, (err) => {
+    res.status(400).send(err);
+  });
 
 });
+
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+      res.send({todos});
+  }, (err) => {
+      res.status(400).send(err);
+  });
+});
+
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
