9ca913e393bca659b0ef0d4712c61c2a83d463db
davidtin <davidtin@opdaconsulting.com>
2018-04-24 16:20:59 -0400

    added POST /resume GET /resume

--- server/models/resume.js
+++ server/models/resume.js
@@ -1,12 +1,78 @@
 var mongoose = require('mongoose');
 
-var ResumeModel = mongoose.model('Resume', {
+// var history = mongoose.model('history',{
+//     period: {
+//       type: String
+//     },
+//     companyname:{
+//       type: String
+//     },
+//     position: {
+//       type: String
+//     },
+//     roles: [{
+//         type: String
+//     }]
+// });
+
+var Resume = mongoose.model('Resume', {
+  Name:{
+    type: String
+  },
+  Address:{
+    type: String
+  },
+  PostalCode:{
+    type: String
+  },
+  Country:{
+    type: String
+  },
+  Mobile:{
+    type: String
+  },
+  Email: {
+    type: String
+  },
+  Linkedin:{
+    type: String
+  },
+  Github:{
+    type: String
+  },
+  Languages: [{
+    language: String,
+    spoken: String,
+    writing: String,
+    listening: String,
+    reading: String
+  }],
   Summary: {
     type: String
   },
-  Skills: {
-    type: Array
-  }
+  Skills: [{
+    type: String
+  }],
+  Employment: [{
+    period: String,
+    companyname: String,
+    position: String,
+    roles: [{
+      type: String
+    }]
+  }],
+  Education:[{
+    year: String,
+    title: String
+  }],
+  Certificates:[{
+    year: String,
+    description: String
+  }],
+  Extracurriculum:[{
+    period: String,
+    description: String
+  }]
 });
 
-module.exports = {ResumeModel};
+module.exports = {Resume};
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var {mongoose} = require('./db/mongoose');
 //import models
 var {Todo} = require('./models/todo');
 //var {User} = require('./models/user');
-//var {ResumeModel} = require('./models/resume');
+var {Resume} = require('./models/resume');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -30,6 +30,41 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
       res.send({todos});
+  }, (err) => {
+      res.status(400).send(err);
+  });
+});
+
+app.post('/resume', (req, res) => {
+  var resume = new Resume({
+    Name: req.body.name,
+    Address: req.body.address,
+    PostalCode: req.body.postalcode,
+    Country: req.body.country,
+    Mobile: req.body.mobile,
+    Email: req.body.email,
+    Linkedin: req.body.linkedin,
+    Github: req.body.github,
+    Languages: req.body.languages,
+    Summary: req.body.summary,
+    Skills: req.body.skills,
+    Employment: req.body.employment,
+    Education: req.body.education,
+    Certificates: req.body.certificates,
+    Extracurriculum: req.body.extracurriculum
+  });
+  resume.save().then((docs) => {
+  res.send(docs);
+}, (err) => {
+    res.status(400).send(err);
+  });
+
+});
+
+
+app.get('/resume', (req, res) => {
+  Resume.find().then((docs) => {
+      res.send({docs});
   }, (err) => {
       res.status(400).send(err);
   });
