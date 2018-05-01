7e62db3cf013c5cb8367fec7c7048d8d66f34063
davidtin <davidtin@opdaconsulting.com>
2018-05-01 18:56:10 -0400

    error on POST /users/login undefined promise return?

--- server/models/user.js
+++ server/models/user.js
@@ -75,6 +75,26 @@ UserSchema.statics.findByToken = function (token) {
     });
 };
 
+//model methods by findByCredentials
+UserSchema.statics.findByCredentials = function(email, password) {
+        var User = this;
+
+        User.findOne({email}).then((user) => {
+          if (!user){
+              return Promise.reject();
+          }
+
+        return new Promise((resolve, reject) => {
+              bcrypt.compare(password, user.password, (err, res) => {
+                  if (res) {
+                    resolve(user);
+                  } else {
+                    reject();
+                  }
+              });//endcompare
+        });//end new promise
+  });//end find one
+};
 
 //use of mongoose middleware pre, always use next as parameter, else program will crash
 UserSchema.pre('save', function (next) {
