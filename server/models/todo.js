d3affa2843bd31c6a688566c706e16cccecc5213
davidtin <davidtin@opdaconsulting.com>
2018-05-02 14:45:13 -0400

    Changed todo and seeds

--- server/models/todo.js
+++ server/models/todo.js
@@ -16,6 +16,10 @@ var Todo = mongoose.model('Todo', {
   },
   schedule: {
     type: String
+  },
+  _creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    required:true
   }
 });
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -17,6 +17,7 @@ describe('POST /todos', () => {
 
     request(app)
       .post('/todos')
+      .set('x-auth', users[0].tokens[0].token)
       .send({text})
       .expect(200)
       .expect((res) => {
@@ -38,6 +39,7 @@ describe('POST /todos', () => {
 it('should not create todo with invalid body data', (done) => {
   request(app)
     .post('/todos')
+    .set('x-auth', users[0].tokens[0].token)
     .send({})
     .expect(400)
     .end((err, res) => {
@@ -58,9 +60,10 @@ describe('GET /todos', () => {
   it('should get all todos', (done) => {
     request(app)
     .get('/todos')
+    .set('x-auth', users[0].tokens[0].token)
     .expect(200)
     .expect((res) => {
-        expect(res.body.todos.length).toBe(2);
+        expect(res.body.todos.length).toBe(1);
     })
     .end(done);
   });
@@ -164,7 +167,7 @@ describe('POST /users/login', () => {
         }
 
         User.findById(users[1]._id).then((user) => {
-          expect(user.tokens[0]).toInclude({
+          expect(user.tokens[1]).toInclude({
             access: 'auth',
             token: res.headers['x-auth']
           });
@@ -191,7 +194,7 @@ describe('POST /users/login', () => {
         }
 
         User.findById(users[1]._id).then((user) => {
-          expect(user.tokens.length).toBe(0);
+          expect(user.tokens.length).toBe(1);
           done();
         }).catch((e) => done(e));
       });
